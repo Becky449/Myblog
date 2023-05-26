8717fbeb95fa9c08483fe2249ba34585da809626
Ayo Moses <ayomoses@afrimash.com>
2023-05-26 08:18:45 +0100

    Implement test for requests on posts index page

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/id/posts/index' do
+    before(:each) do
+      user = User.create!(name: 'Becky', photo: 'https://img.i.com/2.jpg', bio: 'Hey I am Becky.',
+                          postscounter: 1)
+      get "/users/#{user.id}/posts"
+    end
+    it 'check if it brings success response' do
+      expect(response).to be_successful
+    end
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+    it 'check the response body includes correct placeholder text' do
+      expect(response.body).to include('<ul class="photo"> ')
+    end
+  end
+  describe 'GET users from /users/:id to show' do
+    user = User.create!(name: 'Burak', photo: 'https://picsum.photos/200/300', bio: 'I am Burak', postscounter: 4)
+    post = Post.create!(author: user, title: 'title', text: ' Post text 1', commentscounter: 0, likescounter: 0)
+    it 'check if it brings success response' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to be_successful
+    end
+    it 'check the response body includes correct placeholder text' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include(' <div class="post_comment">')
+    end
+    it 'renders the show template' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template('show')
+    end
+  end
+end
